58e1770dd76b82058b02f0659de3eff32fe5c448
Anmol-Baranwal <anmolbaranwal119@gmail.com>
2023-08-03 11:11:26 +0530

    testing gpg verified state

--- components/Aside/Aside.tsx
+++ components/Aside/Aside.tsx
@@ -4,7 +4,7 @@ import { SideNavbarBody } from 'components/SideNavbar/SideNavbarBody'
 export const Aside: FC = () => {
   return (
     <aside className="fixed top-[76px] left-0 z-[20] hidden h-screen w-[290px] lg:block">
-      <SideNavbarBody />
+      <SideNavbarBody />I wanna check gpg
     </aside>
   )
 }
